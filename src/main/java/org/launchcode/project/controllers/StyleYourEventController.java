6e229f16134f18c514370d8e8f92cb84a2cbdaa8
Ben <bensztukowski@gmail.com>
2021-08-18 17:20:42 -0500

    changed to lowercase on claire's controller and html

--- src/main/java/org/launchcode/project/controllers/StyleYourEventController.java
+++ src/main/java/org/launchcode/project/controllers/StyleYourEventController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class StyleYourEventController {
 
-    @GetMapping("/styleYourEvent")
+    @GetMapping("/styleyourevent")
     public String StyleYourEvent(){
 
-        return "styleYourEvent";
+        return "styleyourevent";
     }
 
 
