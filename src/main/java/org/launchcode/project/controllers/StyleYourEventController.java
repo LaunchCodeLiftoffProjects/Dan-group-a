d1bf39678fc67f78b2417a9482b956bc6d8ba450
clairepatt19@gmail.com <clairepatt19@gmail.com>
2021-08-25 17:00:50 -0500

    couple more updates to posting

--- src/main/java/org/launchcode/project/controllers/StyleYourEventController.java
+++ src/main/java/org/launchcode/project/controllers/StyleYourEventController.java
@@ -1,7 +1,9 @@
 package org.launchcode.project.controllers;
 import org.launchcode.project.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -20,10 +22,15 @@ public class StyleYourEventController {
     }
 
     @PostMapping("styleyourevent")
-    public String processPostEventForm(@RequestParam String postEvent){
-        posts.add(new Post(postEvent));
-        return "redirect:";
+    public String processPostEventForm(@ModelAttribute @Valid Post newPost, Error errors, Model model){
 
+        if(errors.hasErrors()){
+            model.addAttribute("title","Create Post")
+            return "styleyourevent";
+        }
+
+        stylestormRepository.save(postEvent);
+        return "redirect:";
     }
 
 
