1597a8cab4ddabd6b449a9d8ace8bb6f9ad7a527
clairepatt19@gmail.com <clairepatt19@gmail.com>
2021-08-29 19:28:57 -0500

    Updating Style Your Event Controller

--- src/main/java/org/launchcode/project/controllers/StyleYourEventController.java
+++ src/main/java/org/launchcode/project/controllers/StyleYourEventController.java
@@ -4,34 +4,42 @@ import org.launchcode.project.models.Post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+
+import javax.validation.Valid;
+import java.util.Optional;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping("styleyourevent")
 public class StyleYourEventController {
 
-    private static List<Post> posts = new ArrayList<>();
+//    private static List<Post> posts = new ArrayList<>();
 
     @Autowired
     private PostRepository postRepository;
 
-    @GetMapping("styleyourevent")
-    public String StyleYourEvent(){
-
-        return "styleyourevent";
+    @GetMapping("post")
+    public String displayCreateEventForm(Model model) {
+        model.addAttribute(new Post());
+        return "styleyourevent/post";
     }
 
-    @PostMapping("styleyourevent")
-    public String processPostEventForm(@ModelAttribute Post newPost, Model model){
+    @PostMapping("post")
+    public String processPostEventForm(@ModelAttribute @Valid Post newPost, Errors errors, Model model){
 
+        if(errors.hasErrors()){
             model.addAttribute("title","Create Post");
-            PostRepository.save(newPost);
-            return "redirect:";
+            model.addAttribute(new Post());
+            return "styleyourevent/post";
+        }
+
+        postRepository.save(newPost);
+        return "redirect:";
     }
 
 
--- src/main/java/org/launchcode/project/models/Tag.java
+++ src/main/java/org/launchcode/project/models/Tag.java
@@ -2,6 +2,7 @@ package org.launchcode.project.models;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
