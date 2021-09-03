26e68f8c77b72d90bae9197b6860a3ac91c920b3
Bri'Anna Tolliver <bri39annat@gmail.com>
2021-09-03 00:26:15 -0500

    changed layout

--- src/main/java/org/launchcode/project/data/TagRepository.java
+++
@@ -1,9 +0,0 @@
-package org.launchcode.project.data;
-
-import org.launchcode.project.models.Tag;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TagRepository extends CrudRepository<Tag, Integer> {
-}
