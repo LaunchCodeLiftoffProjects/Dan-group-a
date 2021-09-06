86c70c4c97db1919e891bfca87dc27225b0bc193
clairepatt19@gmail.com <clairepatt19@gmail.com>
2021-09-06 14:02:46 -0500

    Adding User Class back in

--- src/main/java/org/launchcode/project/data/UserRepository.java
+++ src/main/java/org/launchcode/project/data/UserRepository.java
@@ -1,6 +1,7 @@
 package org.launchcode.project.data;
 
 import org.launchcode.project.models.Tag;
+import org.launchcode.project.models.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
