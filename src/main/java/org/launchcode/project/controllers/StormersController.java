effcda0841c1368961a4143c91124fed79165577
Bri'Anna Tolliver <bri39annat@gmail.com>
2021-09-07 22:55:24 -0500

    enabled the form to submit and redirect to stormers index page

--- src/main/java/org/launchcode/project/controllers/StormersController.java
+++ src/main/java/org/launchcode/project/controllers/StormersController.java
@@ -1,21 +1,37 @@
 package org.launchcode.project.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
+@RequestMapping("stormers")
 public class StormersController {
 
-    @GetMapping("/stormers/index")
-    public String Stormers(){
+    private static List<String> posts = new ArrayList<>();
 
+    @GetMapping("index")
+    public String Stormers(Model model){
+            model.addAttribute("posts", posts);
         return "stormers/index";
     }
 
-    @GetMapping("/stormers/form")
+    @GetMapping("form")
     public String displayStormersForm(){
 
         return "stormers/form";
+    }
+
+    @PostMapping("form")
+    public String processStormersForm(@RequestParam String formsForPosts){
+        posts.add(formsForPosts);
+        return "redirect:index";
     }
 
 
