ba964886ffe5e51c5967492c321c54ae783b9c53
clairepatt19@gmail.com <clairepatt19@gmail.com>
2021-09-25 14:52:08 -0500

    working on post, can't get the form for add tags to pull up, get error 404

--- src/main/java/org/launchcode/project/controllers/StyleYourEventController.java
+++ src/main/java/org/launchcode/project/controllers/StyleYourEventController.java
@@ -2,8 +2,11 @@ package org.launchcode.project.controllers;
 
 import org.launchcode.project.data.PostRepository;
 import org.launchcode.project.data.TagRepository;
+import org.launchcode.project.data.UserRepository;
+import org.launchcode.project.models.DTO.PostTagDTO;
 import org.launchcode.project.models.Post;
 import org.launchcode.project.models.Tag;
+import org.launchcode.project.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,8 +19,11 @@ import java.util.List;
 import java.util.Optional;
 
 @Controller
-@RequestMapping("/styleyourevent")
+@RequestMapping("styleyourevent")
 public class StyleYourEventController {
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Autowired
     private PostRepository postRepository;
@@ -25,77 +31,51 @@ public class StyleYourEventController {
     @Autowired
     private TagRepository tagRepository;
 
-    @GetMapping
-    public String index(Model model) {
+    @GetMapping("")
+    public String displayAllPosts(Model model) {
         model.addAttribute("title", "All Posts");
         model.addAttribute("posts", postRepository.findAll());
         return "styleyourevent/index";
     }
 
-    @GetMapping("styleyourevent/create")
+    @GetMapping("create")
     public String displayCreatePostForm(Model model) {
-        model.addAttribute("title", "Add Post");
+        model.addAttribute("title", "Create Post");
         model.addAttribute(new Post());
+        model.addAttribute("users", userRepository.findAll());
         model.addAttribute("posts", postRepository.findAll());
         model.addAttribute("tags", tagRepository.findAll());
-
         return "styleyourevent/create";
     }
 
-//    @PostMapping("styleyourevent/create")
-//    public String processCreatePostForm(@ModelAttribute @Valid Post newPost, Tag newTag,
-//                                        Errors errors, Model model, @RequestParam int postId, @RequestParam List<Integer> tags) {
-//        if (errors.hasErrors()) {
-//            model.addAttribute("title", "Create Post");
-//            return "styleyourevent/create";
-//        }else {
-//            model.addAttribute("title", "Create Post");
-//            Optional<Post> result = postRepository.findById(postId);
-//            Iterable<Tag> tagResult = tagRepository.findAllById(tags);
-//            if (result.isEmpty()) {
-//                model.addAttribute("title", "Invalid Post ID: " + postId);
-//            } else {
-//                Post post = result.get();
-//                newPost.setPost(post);
-//                model.addAttribute("title", "Posts in tag: " + post.getName());
-//                model.addAttribute("posts", post.getTags());
-//            }
-//                newPost.setTags((List<Tag>) tagResult);
-//
-//            postRepository.save(newPost);
-//            return "redirect:";
-//
-//        }
+    @PostMapping("create")
+    public String processCreatePostForm(@ModelAttribute @Valid Post newPost, Errors errors, Model model, @RequestParam int postId, @RequestParam List<Integer> tags) {
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Create Post");
+            return "styleyourevent/create";
+        } else {
+            model.addAttribute("title", "Create Post");
+            Optional<Post> result = postRepository.findById(postId);
+            Iterable<Tag> tagResult = tagRepository.findAllById(tags);
 
+            newPost.setTags((List<Tag>) tagResult);
 
+            postRepository.save(newPost);
+            return "redirect:";
+
+        }
     }
-
-//    @PostMapping("styleyourevent/add-tag")
-//    public String processAddTagForm(@ModelAttribute @Valid Tag newTag,
-//                                    Errors errors, Model model) {
-//
-//        if (errors.hasErrors()) {
-//            model.addAttribute("title", "Add Tag");
-//            model.addAttribute(new Tag());
-//            return "styleyourevent/add-tag";
-//        }
-//
-//        tagRepository.save(newTag);
-//        return "redirect:";
-//    }
-
+}
 //    @GetMapping("view/{postId}")
-//    public String displayViewPosts(Model model, @PathVariable int postId) {
-//        Optional<Post> optPost = postRepository.findById(postId);
-//        if (optPost.isPresent()) {
-//            Post post = (Post) optPost.get();
-//            model.addAttribute("post", post);
-//            return "styleyourevent/view";
-//        } else {
-//            return "redirect:../";
-//        }
-//    }
+//    public String displayViewPost(Model model, @PathVariable int postId) {
+//        Optional<Post> result = postRepository.findById(postId);
+//        Post post = result.get();
+//        model.addAttribute("post", post);
+//        model.addAttribute("tag", post);
+//        return "styleyourevent/view";
 //
+//
+//    }
 //    @GetMapping(value = "styleyourevent/delete")
 //    public String displayDeletePostForm(Model model) {
 //        model.addAttribute("title", "Delete Posts");
@@ -115,6 +95,7 @@ public class StyleYourEventController {
 //    }
 
 //}
+
 
 
 
---
+++ src/main/java/org/launchcode/project/models/DTO/PostTagDTO.java
@@ -0,0 +1,35 @@
+package org.launchcode.project.models.DTO;
+
+import org.launchcode.project.models.Post;
+import org.launchcode.project.models.Tag;
+
+import javax.validation.constraints.NotNull;
+
+public class PostTagDTO {
+
+    @NotNull
+    private Post post;
+
+    @NotNull
+    private Tag tag;
+
+    public PostTagDTO(){}
+
+    public Post getPost(){
+        return post;
+    }
+
+    public void setPost(Post post){
+        this.post = post;
+    }
+
+    public Tag getTag(){
+        return tag;
+    }
+
+    public void setTag(Tag tag){
+        this.tag=tag;
+    }
+
+
+}
