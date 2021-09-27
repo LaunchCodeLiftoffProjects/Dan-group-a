14006fd00882189173fe46f688bd021f3166216b
Bri'Anna Tolliver <bri39annat@gmail.com>
2021-09-27 17:37:38 -0500

    update

--- src/main/java/org/launchcode/project/data/UserRepository.java
+++
@@ -1,8 +0,0 @@
-//package org.launchcode.project.data;
-//
-//import org.launchcode.project.models.Tag;
-//import org.launchcode.project.models.User;
-//import org.springframework.data.repository.CrudRepository;
-//
-//public interface UserRepository extends CrudRepository<User, Integer> {
-//}
